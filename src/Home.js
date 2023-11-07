c43a9a43bae68523c6fb855ab996f375f5bbdc2a
Vlad Alvarez <moroni.v.alvarez@gmail.com>
2023-11-07 12:59:22 -0700

    added useState hooks, updated CSS

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+    //useState Hook
+    //const [something(name of const), setSomething(name of function) = useState(initial value)]
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName('luigi');
+        setAge('30');
+    }
+
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <p>{ name } is { age } years old</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
