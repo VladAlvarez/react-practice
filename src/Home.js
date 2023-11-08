b0351c374d050ec9d17deff923ad4c52b4f21753
Vlad Alvarez <moroni.v.alvarez@gmail.com>
2023-11-08 11:26:42 -0700

    added BlogList component and props

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     //useState Hook
     //const [something(name of const), setSomething(name of function) = useState(initial value)]
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
-    const handleClick = () => {
-        setName('luigi');
-        setAge('30');
+    const [name, setName] = useState('mario');
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        //sets the new value of blogs to newBlogs
+        setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log('use effect ran');
+        
+    }, []);
 
     return (
         <div className="home">
-            <h2>Home Page</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
+            <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
         </div>
     );
 }
